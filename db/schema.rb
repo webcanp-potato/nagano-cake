4dd6a47bafee2a80b82236de66ff04ae91e55905
EC2 Default User <ec2-user@ip-172-31-92-231.ec2.internal>
2021-12-16 08:48:15 +0000

    oo

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 2021_12_16_054903) do
 
   create_table "cart_products", force: :cascade do |t|
