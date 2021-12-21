d6ab4569b278690fe4fd1fed03de1a7969ce3d34
EC2 Default User <ec2-user@ip-172-31-92-231.ec2.internal>
2021-12-21 03:15:20 +0000

    バリデーション

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -14,8 +14,28 @@ class Admin::ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    @product.save
-    redirect_to admin_products_path
+    if params[:product][:price].present? && params[:product][:name].present? && params[:product][:explanation].present? && params[:product][:genre_id].present? && params[:product][:image].present?
+      # 価格は半角数字0 ~ 9のみ登録可能で、それ以外は登録出来ないようにするif文
+      if params[:product][:price].to_s.ord >= 48 && params[:product][:price].to_s.ord <= 57
+        # 保存に成功した時のif文
+        if @product.save
+          redirect_to admin_products_path
+          flash[:success] = "商品を登録しました"
+        else
+          flash[:danger] = "必要情報を入力してください"
+          render "admin/products/new"
+        end
+      else
+        flash[:danger] = "価格は半角数字で記入してください"
+        render "admin/products/new"
+      end
+    else
+      # 格カラムが空白で保存されなかった場合
+      unless @product.save
+        flash[:danger] = "必要情報を入力してください"
+        render "admin/products/new"
+      end
+    end
   end
 
   def show
