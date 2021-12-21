110b4e560f44659e557318204161154f5515de4f
sho610 <ec2-user@ip-172-31-54-218.ec2.internal>
2021-12-21 02:49:04 +0000

    オーダー

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_20_062043) do
+ActiveRecord::Schema.define(version: 2021_12_20_234239) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -86,6 +86,8 @@ ActiveRecord::Schema.define(version: 2021_12_20_062043) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "customer_id"
+    t.integer "order_status", default: 0, null: false
+    t.integer "payment_method", default: 0, null: false
   end
 
   create_table "products", force: :cascade do |t|
