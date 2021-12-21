98836abdaa768c3dc3f25ce37b12780dd218e2c8
sho610 <ec2-user@ip-172-31-54-218.ec2.internal>
2021-12-21 07:24:05 +0000

    編集

--- app/controllers/customer/products_controller.rb
+++ app/controllers/customer/products_controller.rb
@@ -3,8 +3,17 @@ class Customer::ProductsController < ApplicationController
 before_action :authenticate_customer!, only: [:show]
 
  def index
-    @genres = Genre.all
-    @products = Product.where(sales_status: true).page(params[:page]).per(8)
+    @genres = Genre.where(is_genres_status: true)
+   if params[:genre_id].nil?
+      @products_all = Product.joins(:genre).where(genres: { is_genres_status: true }).where(sales_status: true).order(created_at: :desc)
+      # ジャンルが有効かつ商品も販売中の商品のみ表示させる
+      @products = @products_all.page(params[:page]).per(8)
+      @index = "商品"
+   else
+      @products_all = Product.joins(:genre).where(genres: { is_genres_status: true, id: params[:genre_id] }).where(sales_status: true).order(created_at: :desc)
+      @products = @products_all.page(params[:page]).per(8)
+      @index = Genre.find( params[:genre_id]).name
+   end
  end
 
  def show
