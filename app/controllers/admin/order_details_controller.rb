b597d6c947c8b8e2843e62f192450709121084a6
EC2 Default User <ec2-user@ip-172-31-54-161.ec2.internal>
2021-12-23 06:40:44 +0000

    製作、注文ステータスを連動させた

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -4,10 +4,25 @@ class Admin::OrderDetailsController < ApplicationController
 
   def update
   		@order_detail = OrderDetail.find(params[:id])
+  		@order = @order_detail.order
 		  if @order_detail.update(order_detail_params)
-		  	flash[:success] = "制作ステータスを変更しました"
+		  	flash[:success] = "製作ステータスを変更しました"
+
+		  	if @order_detail.created_status == "製作中" #製作ステータスが「製作中」なら下が入る
+			    @order.update(order_status: 2) #注文ステータスを「製作中」に更新
+
+		# 上記の条件に当てはまらなければ、商品の個数の特定製作ステータスが「製作l完了」の商品をカウント
+	  #  数が一致すれば、全ての商品の製作ステータスが「製作完了」だとわかる
+			elsif @order.order_details.count ==  @order.order_details.where(created_status: "製作完了").count
+			@order.update(order_status: 3) #注文ステータスを「発送準備中]に更新
+
+			elsif @order_detail.created_status == "発送" #製作ステータスが「製作中」なら下が入る
+			    @order.update(order_status: 4) #注文ステータスを「製作中」に更新
+			end
+
 		    redirect_to admin_order_path(@order_detail.order)
 		  else
+
 		     render "show"
 		  end
   end
